fc7e3458b0594a047f0d6a3e8db20870f6cc4706
Manojna <manojna.gopher@gmail.com>
2023-08-19 07:52:01 +0530

    feat: connect DB
    - connect to a postgres database
    - use docker compose
    - pass DB_URI env for server

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 
 FROM golang:1.21-alpine3.18 AS build-stage
 WORKDIR /app
-COPY go.mod ./
+COPY go.mod go.sum ./
+RUN go mod download
 COPY . ./
 RUN go build -o /scount-api
 
--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	_ = http.ListenAndServe(":8080", http.HandlerFunc(handler))
+	uri := os.Getenv("DB_URI")
+	db, err := sql.Open("postgres", uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = http.ListenAndServe(":8080", &Handler{db})
 }
 
-func handler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("Hello World!"))
+type Handler struct {
+	DB *sql.DB
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	var data int
+	row := h.DB.QueryRow("SELECT 5 AS data")
+	err := row.Scan(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	_, _ = fmt.Fprintf(w, "DB Conn successful: data=%d", data)
 }
