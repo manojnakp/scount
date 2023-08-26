d11867018e384c9ea7e92ebb74e52063b0ab68fc
Manojna <manojnakp2003@gmail.com>
2023-08-26 22:20:24 +0530

    feat(api): endpoint `/auth/login`
    - middleware sends BadRequest or UnsupportedMediaType
    - if no matching entry for email id in db or wrong password
      respond UnsupportedEntity
    - handler responds InternalServerError in case of failure

--- api/auth.go
+++ api/auth.go
@@ -45,6 +45,32 @@ type RegisterResponse struct {
 	UserId string `json:"user_id"`
 }
 
+// LoginRequest is the JSON request body format
+// at the `/auth/login` endpoint.
+type LoginRequest struct {
+	// /docs/LoginRequest.schema.json
+	// Schema string `json:"$schema,omitempty"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Validate implements Validator on LoginRequest.
+// Simply check non-empty.
+func (r LoginRequest) Validate() error {
+	if r.Email == "" || r.Password == "" {
+		return errors.New("api: validation failed")
+	}
+	return nil
+}
+
+// LoginResponse is the JSON response body format
+// at the `/auth/login` endpoint.
+type LoginResponse struct {
+	// `/docs/LoginResponse.schema.json`
+	Schema string `json:"$schema,omitempty"`
+	Token  string `json:"token"`
+}
+
 // AuthResource is http.Handler for all requests to `/auth`
 type AuthResource struct {
 	DB *db.Store
@@ -55,6 +81,8 @@ func (res AuthResource) Router() chi.Router {
 	r := chi.NewRouter()
 	r.With(BodyParser[RegisterRequest], Validware[RegisterRequest]).
 		Post("/register", res.register)
+	r.With(BodyParser[LoginRequest], Validware[LoginRequest]).
+		Post("/login", res.login)
 	return r
 }
 
@@ -102,5 +130,46 @@ func (res AuthResource) register(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewEncoder(w).Encode(RegisterResponse{
 		Schema: path.Join("/docs/", "RegisterResponse.schema.json"),
 		UserId: uid,
+	})
+}
+
+// login handles user sign in.
+func (res AuthResource) login(w http.ResponseWriter, r *http.Request) {
+	body := r.Context().Value(BodyKey).(LoginRequest)
+	user, err := res.DB.Users.FindByEmail(r.Context(), body.Email)
+	switch {
+	case errors.Is(err, db.ErrNoRows): // not found
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	case err != nil: // db error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	buf, err := base64.StdEncoding.DecodeString(user.Password)
+	if err != nil { // failed
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = bcrypt.CompareHashAndPassword(buf, []byte(body.Password))
+	switch {
+	// invalid password provided by client in request body
+	case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	case err != nil: // server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	token, err := GenerateToken(user.Uid)
+	if err != nil { // base64 failed to decode
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// json response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(LoginResponse{
+		Schema: path.Join("/docs/", "LoginResponse.schema.json"),
+		Token:  string(token),
 	})
 }
